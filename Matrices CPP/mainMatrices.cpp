cf2a091b9f50aec7f6b7f4bbead2ff7324021f9a
DocLulucifer <lucassen@live.fr>
2022-04-19 18:42:46 +0200

    Début main

--- Matrices CPP/mainMatrices.cpp
+++ Matrices CPP/mainMatrices.cpp
@@ -12,13 +12,38 @@
 using namespace std;
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    char test[] = "M1.txt";
-    /*CMatrices<double> mattest;
+    /*char test[] = "M1.txt";
+    CMatrices<double> mattest;
     mattest = Cfichier::FICLireMatrice(test);
-    mattest.MATAfficherMatrice();*/
+    mattest.MATAfficherMatrice();
     Cfichier::FICLireMatrice(test);
     cout << "Hello World!\n";
-    system("pause");
+    system("pause");*/
+    unsigned int uiBoucleLire;
+    for (uiBoucleLire = 0; uiBoucleLire < argc - 1; uiBoucleLire++) {
+        try {
+            CFichier::FICLireMatrice(argv[uiBoucleLire]);
+        }
+        catch (CException CEXCException) {
+            if(CEXCException.EXCLireErreur == EXCCheminNul){
+                cout << "Le chemin du fichier" + argv[uiBoucleLire] + " est nul !\n";
+                return 1;
+            }
+				
+            if(CEXCException.EXCLireErreur == EXCParserPointeurNul){
+                cout << "Erreur pointeur parser sur le fichier" + argv[uiBoucleLire] + "!\n";
+                return 1;
+            }
+            if(CEXCException.EXCLireErreur == EXCDimLigneNeg){
+                cout << "La dimension de la ligne est négative sur le fichier" + argv[uiBoucleLire] + "!\n";
+                return 1;
+            }
+            if(CEXCException.EXCLireErreur == EXCDimColonneNeg){
+                cout << "La dimension de la colonne est négative sur le fichier" + argv[uiBoucleLire] + "!\n";
+                return 1;
+            }
+        }
+    }
 }
