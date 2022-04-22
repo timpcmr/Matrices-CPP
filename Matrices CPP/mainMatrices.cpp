8964836fee1892d9372552e18023c9f9691ac0d9
timpcmr <98650963+timpcmr@users.noreply.github.com>
2022-04-22 18:04:42 +0200

    fin exceptions parseur

--- Matrices CPP/mainMatrices.cpp
+++ Matrices CPP/mainMatrices.cpp
@@ -21,32 +21,70 @@ int main(int argc, char* argv[])
     Cfichier::FICLireMatrice(test);
     cout << "Hello World!\n";
     system("pause");*/
-    unsigned int uiBoucleLire;
 
-    CMatrices<double>* pMATmatrices[argc - 1];
+    if (argc > 1) {
 
-    for (uiBoucleLire = 0; uiBoucleLire < argc - 1; uiBoucleLire++) {
-        try {
-            pMATmatrices[uiBoucleLire] = Cfichier::FICLireMatrice(argv[uiBoucleLire]);
+        unsigned int uiBoucleLire;
+        CMatrices<double>* pMATmatrices = new CMatrices<double>[argc - 1];
+
+        for (uiBoucleLire = 0; uiBoucleLire < argc - 1; uiBoucleLire++) {
+            try {
+                pMATmatrices[uiBoucleLire] = Cfichier::FICLireMatrice(argv[uiBoucleLire]);
+            }
+            catch (CException CEXCException) {
+
+                //Gestion des exceptions du parseur
+
+                cout << "Erreur parseur : " << endl;
+
+                if (CEXCException.EXCLireErreur() == EXCCheminNul) {
+                    cout << "Le chemin du fichier " << argv[uiBoucleLire] << " est nul !" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCParserPointeurNul) {
+                    cout << "Erreur pointeur sur le fichier " << argv[uiBoucleLire] << "!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCDimLigneNeg) {
+                    cout << "Le nombre de lignes indique est negatif sur le fichier " << argv[uiBoucleLire] << "!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCDimColonneNeg) {
+                    cout << "Le nombre de colonnes indique est negatif sur le fichier " << argv[uiBoucleLire] << "!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCErrTypeMat) {
+                    cout << "Le type de valeurs donnees pour la matrice " << argv[uiBoucleLire] << " n'est pas de type double !" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCFichierNonOuvert) {
+                    cout << "Le chemin donne pour le fichier " << argv[uiBoucleLire] << " n'est pas valide !" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCDimColonneNeg) {
+                    cout << "La dimension de la colonne est négative sur le fichier " << argv[uiBoucleLire] << "!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCColonneDimInf) {
+                    cout << "Le nombre de colonnes indique sur le fichier " << argv[uiBoucleLire] << " est inferieur au nombre de valeurs presentes!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCColonneDimSup) {
+                    cout << "Le nombre de colonnes indique sur le fichier " << argv[uiBoucleLire] << " est superieur au nombre de valeurs presentes!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCLigneDimInf) {
+                    cout << "Le nombre de lignes indique sur le fichier " << argv[uiBoucleLire] << " est inferieur au nombre de valeurs presentes!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCLigneDimSup) {
+                    cout << "Le nombre de lignes indique sur le fichier " << argv[uiBoucleLire] << " est superieur au nombre de valeurs presentes!" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCTokenNulMinuscule) {
+                    cout << "Erreur interne : L'argument passe a la fonction FICMinuscule est nul lors de l'utilisation du fichier  " << argv[uiBoucleLire] << " !" << endl;
+                }
+                if (CEXCException.EXCLireErreur() == EXCTokenNulSuppEspace) {
+                    cout << "Erreur interne : L'argument passe a la fonction FICSupp_Tab_Espace est nul lors de l'utilisation du fichier  " << argv[uiBoucleLire] << " !" << endl;
+                }
+
+                //Gestion fuites memoires
+                delete[] pMATmatrices;
+                return 1;
+            }
         }
-        catch (CException CEXCException) {
-            if(CEXCException.EXCLireErreur == EXCCheminNul){
-                cout << "Le chemin du fichier" + argv[uiBoucleLire] + " est nul !\n";
-                return 1;
-            }
-				
-            if(CEXCException.EXCLireErreur == EXCParserPointeurNul){
-                cout << "Erreur pointeur parser sur le fichier" + argv[uiBoucleLire] + "!\n";
-                return 1;
-            }
-            if(CEXCException.EXCLireErreur == EXCDimLigneNeg){
-                cout << "La dimension de la ligne est négative sur le fichier" + argv[uiBoucleLire] + "!\n";
-                return 1;
-            }
-            if(CEXCException.EXCLireErreur == EXCDimColonneNeg){
-                cout << "La dimension de la colonne est négative sur le fichier" + argv[uiBoucleLire] + "!\n";
-                return 1;
-            }
-        }
+    }
+    else {
+        cout << "Aucun fichier passe en argument !" << endl;
+        return 1;
     }
 }
